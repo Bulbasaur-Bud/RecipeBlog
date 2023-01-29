517161fd2eb083152f84a0e2460d15c82d804891
praphan <praphanhc@gmail.com>
2023-01-29 23:39:25 +0800

    change in main.ejs

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: 'This field is required.'
+    required: "This field is required.",
   },
   image: {
     type: String,
-    required: 'This field is required.'
+    required: "This field is required.",
   },
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
+
+//let see what's going on to the website
